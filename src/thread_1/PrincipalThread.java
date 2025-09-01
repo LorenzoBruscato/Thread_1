389c4b3234d6fedffee8ad548104fdbed2c94d4a
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-09-01 19:49:07 -0300

    feat: Adiciona as classes

---
+++ src/thread_1/PrincipalThread.java
@@ -0,0 +1,10 @@
+package thread_1;
+
+public class PrincipalThread {
+
+    public static void main(String[] args) {
+        System.out.println("Numero de threads:"
+                + Thread.activeCount());
+        System.out.println(Thread.currentThread());
+    }
+}
